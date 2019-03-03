0ebd773736af78a7b0709b7f638eff7ba4528f74
Your Name <you@example.com>
2019-03-03 20:19:24 +0200

    oneSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,48 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+  // Add the following to your existing ready fuction.
+
+constructor(private platform: Platform, private oneSignal: OneSignal, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+  platform.ready().then(() => {
+    statusBar.styleDefault();
+    splashScreen.hide();
+
+    // OneSignal Code start:
+    // Enable to debug issues:
+    // window["plugins"].OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+    var notificationOpenedCallback = function(jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+
+    this.oneSignal.startInit('de69c52c-08b7-4984-8ecf-9f3eec316948', '703322744261');
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+    
+    this.oneSignal.handleNotificationReceived().subscribe(() => {
+     // do something when notification is received
+    });
+    
+    this.oneSignal.handleNotificationOpened().subscribe(() => {
+      // do something when a notification is opened
+    });
+    
+    this.oneSignal.endInit();
+
+    // window["plugins"].OneSignal
+    //   .startInit("de69c52c-08b7-4984-8ecf-9f3eec316948", "YOUR_GOOGLE_PROJECT_NUMBER_IF_ANDROID")
+    //   .handleNotificationOpened(notificationOpenedCallback)
+    //   .endInit();
+  });
+}
 
   initializeApp() {
     this.platform.ready().then(() => {
