8f668641275cf49f8b101e482e7cc03603ef96b4
osher-sade <osher.sade@gmail.com>
2019-03-04 01:24:04 +0200

    finished register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import {RegisterComponent} from "./pages/register/register.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'settings', component: SettingsComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
