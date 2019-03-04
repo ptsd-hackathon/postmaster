5f6a781f92006325666d0de3f638ffc72ecfac49
itaykl <itaykl123@gmail.com>
2019-03-04 04:23:04 +0200

    finish the settings

--- src/app/types.ts
+++ src/app/types.ts
@@ -5,6 +5,65 @@ export type User = {
     password: string
 }
 
+export enum Gender{
+    MALE,
+    FEMALE
+}
+
+export type CustomDate = {
+    day: string
+    month: string
+    year: string
+}
+
+export type SleepingHours = {
+    bedHour: number
+    wakingHour: number
+}
+
+export type Contact = {
+    phoneNumber: string
+    name: string
+}
+
+export type FamilyStatus = {
+    isMarried: boolean
+    numberOfChildren: number
+}
+
+export enum TraumaType{
+    SEX_ASSAULT,
+    ARMY
+}
+
+export type MedicalInformation = {
+    isTaking: boolean
+    drugs: [string]
+}
+
+export type Address = {
+    state: string
+    city: string
+    street: string
+    apartment: string
+}
+
+export type UserInformation = {
+    gender: Gender;
+    phoneNumber: string;
+    initialPanicAttackDate: CustomDate;
+    sleep: SleepingHours;
+    emergencyContacts: Contact[];
+    isShabbatKeeper: boolean;
+    isSmoking: boolean;
+    familyStatus: FamilyStatus;
+    traumaType: TraumaType;
+    medicalInformation: MedicalInformation;
+    address: Address;
+    stressHours: number[];
+    stressfullPlaces: string[];
+}
+
 export type Query = {
     login: boolean;
 }
