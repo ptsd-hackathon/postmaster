cf687ad00dc67eb549df4288d67d078f00c5b4bf
Your Name <you@example.com>
2019-03-04 03:05:32 +0200

    commit for pull

---
+++ src/app/device-information/geolocation-background.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeolocationBackgroundService {
+
+  constructor() { }
+}
